9693158de148327575c220f25a6fb198192a9520
roughezzz <amrafi9@gmail.com>
2019-05-09 22:11:59 +0600

    fourth commit

--- src/api/resources/song/song.controller.js
+++ src/api/resources/song/song.controller.js
@@ -36,5 +36,59 @@ export default {
             console.error(err);
             return res.status(500).send(err);
         }
+    },
+
+    async findOne(req,res){
+        try{
+            const { id } = req.params;
+            const song = await Song.findById(id);
+            if(!song){
+                return res.status(404).json({err: 'Could not find any songs'});
+            }
+            return res.json(song);
+        } catch(err){
+            console.error(err);
+            return res.status(500).send(err);
+        }
+    },
+
+    async delete(req, res){
+        try{
+            const { id } = req.params;
+            const song = await Song.findOneAndRemove({_id: id});
+            if(!song){
+                return res.status(404).json({err: 'Could not find any songs'});
+            }
+            return res.json(song);
+        } catch(err){
+            console.error(err);
+            return res.status(500).send(err);
+        }
+    },
+
+    async update(req, res){
+        try{
+            const { id } = req.params;
+            const schema = Joi.object().keys({
+                title: Joi.string().optional(),
+                url: Joi.string().optional(),
+                rating: Joi.number().integer().min(0).max(5).optional()
+            });
+
+            const {value, error} = Joi.validate(req.body, schema);
+            if(error && error.details){
+                return res.status(400).json(error);
+            }
+
+            const song = await Song.findOneAndUpdate({_id: id}, value, {new: true});
+            if(!song){
+                return res.status(404).json({err: 'Could not find any songs'});
+            }
+
+            return res.json(song);
+        } catch(err){
+            console.error(err);
+            return res.status(500).send(err);
+        }
     }
 }
