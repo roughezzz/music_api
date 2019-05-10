c0a8f3ece6926bf3264b14327ba6ed21e24541d9
roughezzz <amrafi9@gmail.com>
2019-05-10 21:20:08 +0600

    Fifth Commit and documentation of API is added

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import logger from 'morgan';
+import swaggerUI from 'swagger-ui-express';
 import { connect } from './config/db';
 import { restRouter } from './api';
-
+import swaggerDocument from './config/swagger.json';
 const app = express();
 const PORT = 3000;
 
@@ -14,6 +15,7 @@ app.use(logger('dev'));
 
 // app.get('/', (req, res) => res.json({ msg: 'Welcome to Build and Secure Restful APIS' }));
 app.use('/api', restRouter);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument, { explorer: true }));
 app.use((req, res, next) => {
   const error = new Error('Not found');
   error.message = 'Invalid route';
